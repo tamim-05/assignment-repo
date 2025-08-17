fa4e9dffc863a2ec5ea96ec15022857fa5b8f302
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-08-18 00:46:06 +0600

    print int

---
+++ 3-print_int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{int random;
+
+    cout << "enter your number: " << endl;
+    cin >> random;
+
+    cout << "your number is " << random << endl;
+    return 0;
+}
