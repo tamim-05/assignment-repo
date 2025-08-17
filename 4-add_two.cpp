8028423e1cb76f8696d99ad3ed038bad301d1c3e
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-08-18 00:50:11 +0600

    add two num

---
+++ 4-add_two.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{int a,b,sum;
+
+    cout << "enter your numbers: " << endl;
+    cin >> a >> b;
+
+    sum=a+b;
+
+    cout << "addition= " << sum << endl;
+    return 0;
+}
