26ab3809bc23c1bf9696c5d84e3a9847a9ae6083
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-08-17 14:37:51 +0600

    name printing

---
+++ assignment-1_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{string name;
+
+    cout << "enter your name: " << endl;
+    cin >> name;
+
+    cout << "your name is " << name << endl;
+    return 0;
+}
