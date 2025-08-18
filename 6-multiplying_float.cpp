ae6231e2810ecb7fc28c4877b61df83a7400d1ce
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-08-18 21:13:43 +0600

    multiplying float

---
+++ 6-multiplying_float.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{float a,b,m;
+
+    cout << "enter your numbers: " << endl;
+    cin >> a >> b;
+
+    m=a*b;
+
+    cout << "the multiplication is " << m << endl;
+    return 0;
+}
