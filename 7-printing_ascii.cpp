e45b6c4c8df3fda1f3a9b591f3009e4285d01483
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-08-18 21:21:33 +0600

    printing ascii

---
+++ 7-printing_ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{char random;
+
+    cout << "enter your charecter: " << endl;
+    cin >> random;
+
+    cout << "the ascii value is " << int(random)<< endl;
+    return 0;
+}
