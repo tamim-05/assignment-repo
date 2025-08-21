f845cbb31c6b5aec7b05c6254a3a68f1551d4380
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-08-21 23:25:46 +0600

    printing A to Z using loop

---
+++ 21-A-Z_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "printing A to Z using loop: ";
+    for(char i='A'; i<='Z'; i++ ){
+        cout << i << " ";
+    };
+    
+   
+    return 0;
+}
