f74883d5cf42ac82b304f6729d815d1ca4405e06
tamim-05 <tamimadnanarib05@gmail.com.com>
2025-11-29 00:42:37 +0600

    hexadecimal to decimal conversion

---
+++ 73-hex_to_decimal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main() {
+string hexNum;
+cout << "Enter a hexadecimal number: ";
+cin >> hexNum;
+int decimal;
+stringstream ss;
+ss << hex << hexNum;
+ss >> decimal;
+cout << "Decimal: " << decimal << endl;
+return 0;
+}
